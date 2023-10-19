492d8cea1bee4f6ac0ee88e05c5601aad2366c77
harshtiwari <harshtiwari8833@gmail.com>
2023-10-19 14:41:09 +0530

    login and singup

---
+++ app/src/main/java/com/example/budgetbuddy/util/BudgetApplication.kt
@@ -0,0 +1,9 @@
+package com.example.budgetbuddy.util
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BudgetApplication {
+    class NoteApplication : Application()
+}
